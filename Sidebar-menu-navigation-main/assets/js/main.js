2d8ce3b3193d9c4fa106b9c705d13d544fe2be6c
elRakkai-lab <148459154+elRakkai-lab@users.noreply.github.com>
2024-03-01 21:26:45 +0700

    Add files via upload

---
+++ Sidebar-menu-navigation-main/assets/js/main.js
@@ -0,0 +1,77 @@
+// SHOW MENU
+const showMenu = (toggleId, navbarId,bodyId) =>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+
+    if(toggle && navbar){
+        toggle.addEventListener('click', ()=>{
+            // APARECER MENU
+            navbar.classList.toggle('show')
+            // ROTATE TOGGLE
+            toggle.classList.toggle('rotate')
+            // PADDING BODY
+            bodypadding.classList.toggle('expander')
+        })
+    }
+}
+showMenu('nav-toggle','navbar','body')
+
+// LINK ACTIVE COLOR
+const linkColor = document.querySelectorAll('.nav__link');   
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+
+
+
+//////////////// UNTUK MENGGANTI ICON ////////////////
+
+const navIcon = document.querySelector('.nav__icon');
+
+function handleNavIconClick() {
+    if (navIcon.classList.contains('active')) {
+        navIcon.classList.remove('active');
+        navIcon.style.color = '';
+    } else {
+        navIcon.classList.add('active');
+        navIcon.style.color = 'white';
+    }
+}
+
+if (window.innerWidth <= 690) {
+    navIcon.addEventListener('click', handleNavIconClick);
+}
+
+
+
+//////////////////////////// Filter ////////////////////////////
+
+const categoryTitle = document.querySelectorAll('.category-title');
+const allCategoryPosts = document.querySelectorAll('.kabeh');
+
+for(let i = 0; i < categoryTitle.length; i++){
+    categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
+}
+
+function filterPosts(item){
+    changeActivePosition(item);
+    for(let i = 0; i < allCategoryPosts.length; i++){
+        if(allCategoryPosts[i].classList.contains(item.attributes.id.value)){
+            allCategoryPosts[i].style.display = "block";
+        } else {
+            allCategoryPosts[i].style.display = "none";
+        }
+    }
+}
+
+function changeActivePosition(activeItem){
+    for(let i = 0; i < categoryTitle.length; i++){
+        categoryTitle[i].classList.remove('active');
+    }
+    activeItem.classList.add('active');
+};
